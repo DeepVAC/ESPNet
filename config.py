15b431b189af2d08fa7f957a28d95c0326b9f7eb
wyh <wangyuhang@deepait.com>
2021-05-21 21:16:29 +0800

    update config

--- config.py
+++ config.py
@@ -12,6 +12,32 @@ from data.dataloader import FileLineCvSegWithMetaInfoDataset
 from modules.model import EESPNet_Seg
 from aug.aug import *
 
+## -------------------- datasets & aug ------------------
+config.sample_path_prefix = './data'
+config.datasets.FileLineCvSegWithMetaInfoDataset = AttrDict()
+config.datasets.FileLineCvSegWithMetaInfoDataset.cached_data_file = 'clothes.p'
+config.datasets.FileLineCvSegWithMetaInfoDataset.classes = config.core.cls_num
+config.datasets.FileLineCvSegWithMetaInfoDataset.norm_val = 1.10
+load_data = FileLineCvSegWithMetaInfoDataset(config, './data/train.txt', config.sample_path_prefix)
+config.data = load_data()
+config.datasets.FileLineCvSegDataset = AttrDict()
+
+config.aug.ImageWithMasksNormalizeAug = AttrDict()
+config.aug.ImageWithMasksNormalizeAug.mean = config.data['mean']
+config.aug.ImageWithMasksNormalizeAug.std = config.data['std']
+
+config.aug.ImageWithMasksToTensorAug = AttrDict()
+config.aug.ImageWithMasksToTensorAug.scale = 1
+
+config.aug.ImageWithMasksRandomRotateAug = AttrDict()
+config.aug.ImageWithMasksRandomRotateAug.max_angle = 45
+config.aug.ImageWithMasksRandomRotateAug.fill_color = True
+
+
+config.aug.ImageWithMasksScaleAug = AttrDict()
+config.aug.ImageWithMasksScaleAug.w = 384
+config.aug.ImageWithMasksScaleAug.h = 384
+
 ## ------------------ common ------------------
 config.core.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 config.core.output_dir = 'output'
@@ -30,7 +56,7 @@ config.core.save_num = 1
 config.core.cls_num = 4
 config.core.shuffle = True
 config.core.batch_size = 16
-config.core.model_path = "your model path"
+# config.core.model_path = "your model path"
 
 ## -------------------- tensorboard ------------------
 # config.core.tensorboard_port = "6007"
@@ -43,14 +69,7 @@ config.core.model_path = "your model path"
 #config.cast.TraceCast.static_quantize_dir = "./script.sq"
 # config.cast.TraceCast.dynamic_quantize_dir = "./quantize.sq"
 
-config.sample_path_prefix = './data'
 
-config.datasets.FileLineCvSegWithMetaInfoDataset = AttrDict()
-config.datasets.FileLineCvSegWithMetaInfoDataset.cached_data_file = 'clothes.p'
-config.datasets.FileLineCvSegWithMetaInfoDataset.classes = config.core.cls_num
-config.datasets.FileLineCvSegWithMetaInfoDataset.norm_val = 1.10
-load_data = FileLineCvSegWithMetaInfoDataset(config, './data/train.txt', config.sample_path_prefix)
-config.data = load_data()
 
 ## -------------------- net and criterion ------------------
 config.core.net = EESPNet_Seg(4)
@@ -75,24 +94,6 @@ config.core.scheduler = optim.lr_scheduler.LambdaLR(config.core.optimizer, lr_la
 ## -------------------- loader ------------------
 config.core.num_workers = 2
 
-## -------------------- datasets & aug ------------------
-config.aug.ImageWithMasksNormalizeAug = AttrDict()
-config.aug.ImageWithMasksNormalizeAug.mean = config.data['mean']
-config.aug.ImageWithMasksNormalizeAug.std = config.data['std']
-
-config.aug.ImageWithMasksToTensorAug = AttrDict()
-config.aug.ImageWithMasksToTensorAug.scale = 1
-
-config.aug.ImageWithMasksRandomRotateAug = AttrDict()
-config.aug.ImageWithMasksRandomRotateAug.max_angle = 45
-config.aug.ImageWithMasksRandomRotateAug.fill_color = True
-
-
-config.aug.ImageWithMasksScaleAug = AttrDict()
-config.aug.ImageWithMasksScaleAug.w = 384
-config.aug.ImageWithMasksScaleAug.h = 384
-
-config.datasets.FileLineCvSegDataset = AttrDict()
 #just for fool deepvac
 config.datasets.FileLineCvSegDataset.composer = ESPNetTrainComposer(config)
 config.core.train_dataset = FileLineCvSegDataset(config, './data/train.txt', ',', config.sample_path_prefix)
@@ -150,5 +151,3 @@ scale4_train_loader = torch.utils.data.DataLoader(FileLineCvSegDataset(scale4_co
 scale4_train_loader.is_last_loader = False
 
 config.core.train_loader_list = [scale1_train_loader, scale2_train_loader, scale4_train_loader, scale3_train_loader, last_train_loader]
-
-
