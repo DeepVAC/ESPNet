cee5a63b161b61b08db9e6e3074f2c4873d78179
Gemfield <gemfield@civilnet.cn>
2021-05-24 14:28:32 +0800

    enhance aug

--- aug/aug.py
+++ aug/aug.py
@@ -11,7 +11,10 @@ from config import config
 class ESPNetTrainComposer(Composer):
     def __init__(self, deepvac_config):
         super(ESPNetTrainComposer, self).__init__(deepvac_config)
-        ac1 = AugFactory('ImageWithMasksRandomRotateAug@0.6 =>  ImageWithMasksCenterCropAug => ImageWithMasksScaleAug => ImageWithMasksHFlipAug@0.5 => ImageWithMasksNormalizeAug => ImageWithMasksToTensorAug', deepvac_config)
+        ac1 = AugFactory('SpeckleAug@0.1 => GaussianAug@0.1 => HorlineAug@0.1 => VerlineAug@0.1 => LRmotionAug@0.1 => UDmotionAug@0.1 \
+            => NoisyAug@0.1 => DarkAug@0.1 => ColorJitterAug@0.25 => BrightnessJitterAug@0.25 => ContrastJitterAug@0.25 => \
+            ImageWithMasksRandomRotateAug@0.6 =>  ImageWithMasksCenterCropAug => ImageWithMasksScaleAug => ImageWithMasksHFlipAug@0.5 => \
+            ImageWithMasksNormalizeAug => ImageWithMasksToTensorAug', deepvac_config)
         self.addAugFactory('ac1', ac1)
 
 class ESPNetValComposer(Composer):
